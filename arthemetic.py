72ad1b7e8cd04c45baffaf8d39ff47d451bc675a
ananyashet2007 <ananyashet191@gmail.com>
2025-11-10 16:31:18 +0530

    Create arthemetic.py

---
+++ arthemetic.py
@@ -0,0 +1,21 @@
+a= int( input( "enter first number"))
+b= int(input( "enter second number"))
+result = a + b
+print( "the sum is ", result)
+enter first number8
+enter second number3
+the sum is  11
+
+
+a= int (input ("enter first number "))
+b=  int (input ( "enter second number"))
+add = a+ b
+sub = a- b
+mul = a* b
+div = a/b
+div1=a//b
+modulus = a% b
+print(add, sub ,mul ,div ,div1 ,modulus)
+enter first number 80
+enter second number20
+100 60 1600 4.0 4 0
