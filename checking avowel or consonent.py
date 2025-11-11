57df28d0433ddf7f87296279ab36a879fa903876
ananyashet2007 <ananyashet191@gmail.com>
2025-11-11 15:31:52 +0530

    Fix vowel check and correct spelling of 'consonant'

---
+++ checking avowel or consonent.py
@@ -0,0 +1,10 @@
+# programe for checking vowel or consonent
+c =input("enter the character:")
+if c in "aeiou" or "AEIOU ":
+  print("vowel")
+else:
+  print(" consonent")
+output-
+
+enter the character: O
+vowel
