2381e4d86ae1c3a0f7907ee4b10df828b0a61850
ananyashet2007 <ananyashet191@gmail.com>
2025-11-11 15:30:30 +0530

    Create finding the leap year.py

---
+++ finding the leap year.py
@@ -0,0 +1,9 @@
+# program for leap year
+a = int(input("enter the year"))
+if a % 4==0:
+  print("It is a leap year")
+else:
+  print(" It is not leap year")
+output-enter the year 2028
+It is a leap year
+
