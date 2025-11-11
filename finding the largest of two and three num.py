90af85900bc2a413af2a4af60b8cf21e7565afe5
ananyashet2007 <ananyashet191@gmail.com>
2025-11-11 15:27:28 +0530

    Create  finding the largest of two and three num.py

---
+++ finding the largest of two and three num.py
@@ -0,0 +1,31 @@
+# programe for largest of two numbers
+num1 =int(input("enter the first number"))
+num2=int(input("enter the nsecond number"))
+if num1 > num2:
+   print("the largest number is ",num1)
+elif num2 >num1:
+   print("the largest number is",num2)
+else:
+  print("both are equal")
+  output-enter the first number 8
+enter the nsecond number 19
+the largest number is 19
+
+
+# program for largest of three numbers
+num1=int(input("enter the first number"))
+num2=int(input('enter the second number'))
+num3=int(input('enter the third number'))
+if (num1 >=num2) and (num1>=num3):
+   largest = num1
+elif (num2>=num1) and (num2>=num3):
+   largest = num2
+else:
+   largest = num3
+print( "the largest num is ",largest)
+output-
+
+enter the first number 2
+enter the second number 6
+enter the third number 0
+the largest num is  6
