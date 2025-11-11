ddf26b8b0444cf5021c0e2018bc33eaeaa9e8ba7
ananyashet2007 <ananyashet191@gmail.com>
2025-11-11 15:38:41 +0530

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,39 @@
+for i in range(1 ,10):
+  if i%2==0:
+    print( i,"even number")
+  else:
+    print(i,"odd number")
+    output-
+    
+
+1 odd number
+2 even number
+3 odd number
+4 even number
+5 odd number
+6 even number
+7 odd number
+8 even number
+9 odd number
+
+for a in range(-5,5):
+  if a>0:
+    print(a,"is the positive number")
+  elif a<0:
+    print(a,"is the negative number")
+  else:
+    print(a,"is zero")
+    0utput-
+    
+
+-5 is the negative number
+-4 is the negative number
+-3 is the negative number
+-2 is the negative number
+-1 is the negative number
+0 is zero
+1 is the positive number
+2 is the positive number
+3 is the positive number
+4 is the positive number
+
