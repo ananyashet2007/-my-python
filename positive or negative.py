164c534d56736eddb18961840621209967a11023
ananyashet2007 <ananyashet191@gmail.com>
2025-11-11 15:25:09 +0530

    Create positive or negative.py

---
+++ positive or negative.py
@@ -0,0 +1,10 @@
+# positive, negative or zero
+p = int(input("enter the number"))
+if p > 0:
+  print("positive")
+elif p < 0:
+  print('negative')
+else:
+  print("zero")
+  output- enter the number -1
+negative
