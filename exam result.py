6e7db75814311f0624439f6eb4a5bc6ddb102bd9
ananyashet2007 <ananyashet191@gmail.com>
2025-11-11 15:23:07 +0530

    Create exam result.py

---
+++ exam result.py
@@ -0,0 +1,17 @@
+# exam result
+marks =int(input("enter the marks"))
+if marks>=90:
+  print("grade A+")
+elif marks>=70:
+  print("grade A ")
+elif marks>=50:
+  print("grade B+")
+elif marks>=35:
+  print("grade B ")
+else:
+  print("grade C ,fail")
+output-
+
+enter the marks 40
+grade B 
+
