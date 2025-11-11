825f20e45c99a8cf0d2f930234b9f18c29b4b3a7
ananyashet2007 <ananyashet191@gmail.com>
2025-11-11 15:29:07 +0530

    Create voting eligibility .py

---
+++ voting eligibility .py
@@ -0,0 +1,11 @@
+# program for voting eligibility
+age=int(input("enter your age"))
+if age>=18:
+  print("eligible for voting")
+else:
+  print("not eligible for voting")
+output-enter your age 26
+eligible for voting
+
+
+
