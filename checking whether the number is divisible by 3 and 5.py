ea77c74bee5301415c5d1a7af60ac414b34e4a9e
ananyashet2007 <ananyashet191@gmail.com>
2025-11-11 15:34:01 +0530

    Create checking whether the number is divisible by 3 and 5.py

---
+++ checking whether the number is divisible by 3 and 5.py
@@ -0,0 +1,9 @@
+# program for if the number is divisible by 3 and 5
+num = int(input("enter the num"))
+if num % 3==0 and num %5==0:
+  print("It is divisible by  both 3 and 5")
+else:
+  print("It is not a divisible by both 3 and 5")
+output-
+enter the num 6
+It is not a divisible by 3 and 5
