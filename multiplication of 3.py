8e431085143f9bc3cd9758917361a212b2105b48
ananyashet2007 <ananyashet191@gmail.com>
2025-11-12 11:56:45 +0530

    Create multiplication of 3.py

---
+++ multiplication of 3.py
@@ -0,0 +1,16 @@
+# multiplication table
+num=int(input("enter the number"))
+for i in range(1,11):
+  print(f"{num} x {i}= {num *i}")
+output-
+enter the number 8
+8 x 1= 8
+8 x 2= 16
+8 x 3= 24
+8 x 4= 32
+8 x 5= 40
+8 x 6= 48
+8 x 7= 56
+8 x 8= 64
+8 x 9= 72
+8 x 10= 80
