081f8fe05f6e74b4cd2fa34b4613f362ddd4b9e1
ananyashet2007 <ananyashet191@gmail.com>
2025-11-12 11:16:00 +0530

    Create formatted string.py

---
+++ formatted string.py
@@ -0,0 +1,10 @@
+
+# formatted string
+name="Ananya"
+age=18
+college="shridevi"
+branch="AIML"
+print(f"My name is {name},my age is {age}, I am studing in {college} in {branch} branch ")
+output-
+
+My name is Ananya,my age is 18, I am studing in shridevi in AIML branch 
