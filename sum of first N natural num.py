4399627bea2fc17b7e99f3a561fc7e5c53f678a0
ananyashet2007 <ananyashet191@gmail.com>
2025-11-12 09:07:45 +0530

    Create sum of first N natural num.py

---
+++ sum of first N natural num.py
@@ -0,0 +1,15 @@
+N= int(input("Enter the positive integer"))
+sum=0
+i=1
+while i<=N:
+  sum+=i
+  i+=1
+  print("The sum of first", N ,"natural numbers is" ,sum)
+
+output-Enter the positive integer 6
+The sum of first 6 natural numbers is 1
+The sum of first 6 natural numbers is 3
+The sum of first 6 natural numbers is 6
+The sum of first 6 natural numbers is 10
+The sum of first 6 natural numbers is 15
+The sum of first 6 natural numbers is 21
