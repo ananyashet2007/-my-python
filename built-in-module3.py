116975a571020050f8c311ae73121512beb7faf6
ananyashet2007 <ananyashet191@gmail.com>
2025-11-14 12:10:31 +0530

    Create built-in-module3.py

---
+++ built-in-module3.py
@@ -0,0 +1,9 @@
+from math import sqrt , sin,log,factorial
+print(factorial(7))
+print(sqrt(5))
+print(log(8))
+print(sin(100))
+output-5040
+2.23606797749979
+2.0794415416798357
+-0.5063656411097588
